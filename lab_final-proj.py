e6228fe99784f004324b96dafc24f8d57a8835c7
juna21-meet <juna21@meet.mit.edu>
2019-08-04 10:24:39 -0400

    Stopping here

---
+++ lab_final-proj.py
@@ -0,0 +1,18 @@
+#question 1
+print("do i like CS?")
+answer_1 = input("a) yes\nb)no\nc)sometimes\nd)mybe\n:")
+if answer_1.lower() == "b" or answer_1.lower() == "no":
+    print("Correct")
+else:
+    print("nah, that is wrong")
+
+
+#question 2
+print("are we done?")
+answer_2 = input("a)barely\nb)no\nc)yes!\nd)mybe\n:")
+if answer_2.lower() == "a" or answer_2.lower() == "no":
+    print("Correct")
+else:
+    print("I wish")
+    
+      
